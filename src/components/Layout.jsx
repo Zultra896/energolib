4cd5658503fb3bf630738e3cec385a6a8a196dbe
Aibek <aibekaskarov006@mail.ru>
2024-11-15 08:50:43 +0500

    + layout new

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,1 +1,16 @@
+import { Outlet } from 'react-router-dom';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import layoutStyles from '../css/layout.module.css'
 
+const Layout = () => (
+  <div className={layoutStyles.app}>
+    <Header />
+    <main className={layoutStyles.app__content}>
+      <Outlet />
+    </main>
+    <Footer />
+  </div>
+);
+
+export default Layout;
