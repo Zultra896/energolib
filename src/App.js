6c48488e44566cbd39d832ea08c909dce618d058
Aibek <aibekaskarov006@mail.ru>
2024-11-15 09:33:35 +0500

    + mainPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+import { useRoutes } from 'react-router';
+import { useEffect } from'react';
 import Layout from './components/Layout.jsx';
-import BannerBlock from './components/BannerBlock.jsx' 
+import Main from './pages/Main.jsx'
 
+
+
+
+const routes = [
+  {  
+  path: '/',
+  element: <Layout />,
+  children: [
+    {
+      index: true,
+      element: <Main />,
+    },
+  ]
+  }
+]
 
 function App() {
+
+  useEffect(() => {
+    document.body.style.backgroundColor = '#2C3035';
+  
+    return () => {
+      document.body.style.backgroundColor = ''; 
+    };
+  }, []);
+
+  const elements = useRoutes(routes);
   return (
-    <div className="App">
-      <Layout />
+    <div>
+      {elements}
     </div>
   );
 }
---
+++ src/pages/Main.jsx
@@ -0,0 +1,10 @@
+import BannerBlock from '../components/BannerBlock.jsx'
+import BlockHistory from '../components/BlockHistory.jsx'
+export default function Main() {
+  return (
+    <div>
+        <BannerBlock />
+        <BlockHistory />
+    </div>
+  )
+}
